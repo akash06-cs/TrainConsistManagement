e4b549391b47a11d1350919d3af8a37559653a8c
siveshsp <siveshcs@gmail.com>
2026-03-24 14:29:59 +0530

    UC1: Initialize Train Consist and display initial state

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("=== Train Consist Management App ===");
+
+        List<String> consist = new ArrayList<>();
+
+        System.out.println("Train initialized.");
+        System.out.println("Initial bogie count: " + consist.size());
+
+    }
+}
